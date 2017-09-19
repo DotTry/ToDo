24acee226f27bf972dac44682cf06af48041a3d2
Binh Tran <o.otuto@gmail.com>
2017-09-19 16:58:12 -0500

    Upload

--- src/App.js
+++ src/App.js
@@ -1,98 +1,215 @@
 import React, { Component } from 'react';
-import fire, {auth, provider} from './fire.js';
+import './App.css';
+import firebase, { auth, provider, login2, logout2 } from './firebase.js';
 
-var c = 10;
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.login = this.login.bind(this); // <-- add this line
-    this.logout = this.logout.bind(this); // <-- add this line
-    //this.deleteMessage = this.deleteMessage(this);
-    
-    this.state = { 
-      messages: [],
+  constructor() {
+    super();
+    //Initialize values
+    this.state = {
       currentItem: '',
       username: '',
       items: [],
-      user: null // <-- add this line
-    }; // <- set up react state
-  }
-  componentWillMount(){
-    /* Create reference to messages in Firebase Database */
-    let messagesRef = fire.database().ref('messagesNum').orderByKey().limitToLast(100);
-    messagesRef.on('child_added', snapshot => {
-      /* Update React state when message is added at Firebase Database */
-      let message = { text: snapshot.val(), id: snapshot.key };
-      this.setState({ messages: [message].concat(this.state.messages) });
-    })
-  }
-  addMessage(e){
-    e.preventDefault(); // <- prevent form submit from reloading the page
-    /* Send the message to Firebase */
-    fire.database().ref('messagesNum').push( {value:this.inputEl.value, count:c++} );
-    this.inputEl.value = ''; // <- clear the input
+      user: null,
+      viewall: false
+    }
+    //Bind functions
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.loginSubmit = this.loginSubmit.bind(this);
+    this.logout = this.logout.bind(this);
+    this.viewAll = this.viewAll.bind(this);
+    this.taskDone = this.taskDone.bind(this);
   }
   
-  /*deleteMessage(e){
-    console.log("ID: " + this.state.messages[0].id)
-    fire.database().ref(this.state.messages[0].id).remove();
-  }*/
-  
+  //Change text form
   handleChange(e) {
-    /* ... */
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
+  //firebase logout call
   logout() {
-    // we will add the code for this in a moment, but need to add the method now or the bind will throw an error
-  }
-  login() {
-    auth.signInWithPopup(provider) 
-      .then((result) => {
-        const user = result.user;
+    auth.signOut()
+      .then(() => {
         this.setState({
-          user
+          user: null
         });
       });
   }
+  //Invert viewall flag
+  viewAll(){
+      this.setState({
+            viewall: !this.state.viewall
+          });
+
+  }
+  
+  //////////NEW EDIT
+  state = { loginMessage: null }
+  loginSubmit = (e) => {
+    this.setState(setErrorMsg(null))
+    e.preventDefault()
+    login2(this.email.value, this.pw.value) //Call firebase login
+      .catch((error) => {
+          this.setState(setErrorMsg('Invalid username/password.'))
+        })
+  }
+  //submission of task. 
+  handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items'); //fetch database items query
+    const item = {
+      title: this.state.currentItem,
+      user: this.state.user.displayName || this.state.user.email,
+      done: false
+    }
+    itemsRef.push(item); //push the task. Task will have author data.
+    
+    this.setState({//Reset state for next task to be added. Text forms will be cleared
+      currentItem: '',
+      username: ''
+    });
+  }
+  
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => { //If current logged in user changes, update our state.
+      if (user) {
+        this.setState({ user });
+      } 
+    });
+    const itemsRef = firebase.database().ref('items'); //Fetch our database for our todo activities
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title: items[item].title,
+          user: items[item].user,
+          done: items[item].done
+        });
+      }
+      this.setState({
+        items: newState
+      });
+    });
+  }
+  removeItem(itemId) { //Find entry and remove activity
+    const itemRef = firebase.database().ref(`/items/${itemId}`);
+    itemRef.remove();
+  }
+  taskDone(itemId){ //update our entry with the done status
+    const itemRef = firebase.database().ref(`/items/${itemId}`);
+    console.log(itemRef);
+    itemRef.update({done:true});
+  }
   
   render() {
-    console.log(this.state.messages)
-    const listItems = this.state.messages.map((todo) =>
-            <li>
-              {todo.text.value}
-            </li>
-          )
-    //for( var t = 0; t < this.state.messages.length; t++ )
-    //    console.log(this.state.messages[t].text.value + "??")
     return (
-      <div>
-      <form onSubmit={this.addMessage.bind(this)}>
-        <input type="text" ref={ el => this.inputEl = el }/>
-        <input type="submit"/>
-        <ul>
-          {
-          listItems
-            //this.state.messages.map( message => <li key={message[0]}>{message[0].value}</li> )
-            //for( var t in this.state.messages )
-            //  console.log(this.state.messages[t].text.value)
-            //console.log(this.state.messages)
+      <div className='app'>
+        <header>
+            <div className="wrapper">
+              <h2>To Do With Friends</h2>
+              
+              
+              
+          {this.state.user ?
+                
+                <button type="submit" onClick={this.logout} className="btn btn-primary">Logout</button>    
+                
+                :
+                
+                <form onSubmit={this.loginSubmit}>
+                  <div className="form-group">
+                    <label>Email</label>
+                    <input className="form-control" ref={(email) => this.email = email} placeholder="Email"/>
+                  </div>
+                  <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Password" ref={(pw) => this.pw = pw} />
+                  </div>
+                  {
+                    this.state.loginMessage &&
+                    <div className="alert alert-danger" role="alert">
+                      <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                      <span className="sr-only">Error:</span>
+                      &nbsp;{this.state.loginMessage} <a href="#" onClick={this.resetPassword} className="alert-link">Forgot Password?</a>
+                    </div>
+                  }
+                  
+                  {this.state.user ?
+                <button type="submit" onClick={this.logout} className="btn btn-primary">Logout</button>                
+                :
+                <button type="submit" className="btn btn-primary">Log In</button>              
+                }
+              
+                </form>
           }
-        </ul>
-        
-      </form>
-      
-        
-      <div className="wrapper">
-        <h1>Fun Food Friends</h1>
+                
+                
+                
+                
+            </div>
+        </header>
         {this.state.user ?
-          <button onClick={this.logout}>Log Out</button>                
-          :
-          <button onClick={this.login}>Log In</button>              
-        }
-      </div>
+          <div>
+            <div className='user-profile'>
+                <img src={this.state.user.photoURL} />
+            </div>
+            <div className='container'>
+              <section className='add-item'>
+                    <form onSubmit={this.handleSubmit}>
+                      <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.user.displayName || this.state.user.email} />
+                      <input type="text" name="currentItem" placeholder="What to do?" onChange={this.handleChange} value={this.state.currentItem} />
+                      <button>Add Item</button>
+                    </form>
+              </section>
 
+              <section className='display-item'>
+                  <div className="wrapper">
+                    <ul> 
+                      {this.state.items.map((item) => {
+                      //Filter tasks by author
+                      if(this.state.viewall && item.user === this.state.user.email || !(this.state.viewall))
+                        return (
+                          <li key={item.id}>
+                          
+                          {item.done ? <h1 >Done</h1> : <h1 > Not Done {item.user === this.state.user.displayName || item.user === this.state.user.email ?
+                                <button onClick={() => this.taskDone(item.id)}> Finish</button>
+                              : null}</h1> }
+                          
+                          
+                          
+                            <h3>{item.title}</h3> 
+                            <p>Author: {item.user}
+                              {item.user === this.state.user.displayName || item.user === this.state.user.email ?
+                                <button onClick={() => this.removeItem(item.id)}><i className="fa fa-times" aria-hidden="true"></i> Remove Item</button>
+                              : null}
+                            </p>
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  </div>
+              </section>
+            </div>
+          </div>
+        : 
+          <p>You must be logged in to see the list and submit to it.</p>
+        }
+        <div className="footer"> <button onClick={this.viewAll}>View All Tasks</button>
+        {this.state.viewall ? <span >ON</span> :<span > OFF</span> }
+        
       </div>
-      
+      </div>
     );
+  }
+}
+
+function setErrorMsg(error) {
+  return {
+    loginMessage: error
   }
 }
 
